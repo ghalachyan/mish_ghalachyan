cc0e177a823621211ce32051aac5de51dc4eede1
Misha Ghalachyan <mish.ghalachyan@gmail.com>
2024-09-02 13:03:32 +0400

    I made change the routes and controller of the books.

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -1,5 +1,8 @@
+// import {Op} from "sequelize";
 import Books from '../models/Books.js';
 import Users from '../models/Users.js';
+import Category from '../models/Category.js';
+import BookCategory from "../models/BookCategory.js";
 
 export default {
     async createBook(req, res) {
@@ -57,15 +60,25 @@ export default {
 
     async getBooks(req, res) {
         try {
+            const {id: userId} = req.user;
             const total = await Books.count();
 
             const order = req.query.order;
-            const  orderBy = req.query.orderBy;
+            const orderBy = req.query.orderBy;
             let page = +req.query.page;
             let limit = +req.query.limit;
             let offset = (page - 1) * limit;
 
             const maxPageCount = Math.ceil(total / limit)
+
+            const userExists = await Users.findByPk(userId);
+
+            if (!userExists) {
+                res.status(404).json({
+                    message: 'User not found.',
+                })
+                return;
+            }
 
             if (page > maxPageCount) {
                 res.status(404).json({
@@ -82,7 +95,7 @@ export default {
                 ],
                 offset,
                 limit,
-                order:[
+                order: [
                     [orderBy, order]
                 ],
             });
@@ -106,4 +119,101 @@ export default {
         }
     },
 
+    // async searchBooks(req, res) {
+    //     try {
+    //         const {id: userId} = req.user;
+    //         const {title, author, category} = req.body;
+    //         const result = {};
+    //
+    //         const userExists = await Users.findByPk(userId);
+    //         if (!userExists) {
+    //             res.status(404).json({
+    //                 message: 'User not found.',
+    //             })
+    //             return;
+    //         }
+    //
+    //         if (title) {
+    //             result.title = {[Op.iLike]: `%${title}%`};
+    //         }
+    //
+    //         if (author) {
+    //             result.author = {[Op.iLike]: `%${author}%`};
+    //         }
+    //
+    //         if (category) {
+    //             result.category = {[Op.iLike]: `%${category}%`};
+    //         }
+    //
+    //         const books = await Books.findAll({
+    //             where: result,
+    //             include: [
+    //                 {model: Users}
+    //             ]
+    //         });
+    //
+    //         if (books.length > 0) {
+    //             return res.status(200).json({
+    //                 message: 'Books retrieved successfully', books
+    //             });
+    //         }
+    //         res.status(404).json({
+    //             message: 'No books found'
+    //         });
+    //     } catch (e) {
+    //         res.status(500).json({
+    //             message: 'Internal server error',
+    //             error: e.message,
+    //         });
+    //     }
+    // },
+
+    async bookCategory(req, res) {
+        try {
+            const userId = req.user.id;
+            const bookId = req.params.bookId;
+            const categoryId = req.params.categoryId;
+
+            console.log(userId, bookId, categoryId);
+            const userExists = await Users.findByPk(userId);
+            const bookExists = await Books.findByPk(bookId);
+            const categoryExists = await Category.findByPk(categoryId[0]);
+            console.log(categoryExists);
+            if (!userExists || !bookExists) {
+                res.status(404).json({
+                    message: 'User or book not found.',
+                })
+                return;
+            }
+
+            if (!categoryExists) {
+                res.status(404).json({
+                    message: 'Category not found.',
+                })
+                return;
+            }
+
+            const [bookCategory, created] = await BookCategory.findOrCreate({
+                where: {bookId, categoryId},
+            });
+
+            if (!created) {
+                res.status(400).json({
+                    message: 'Category already mentioned in the Book',
+                });
+                return;
+            }
+
+            res.status(201).json({
+                message: 'Book successfully marked as favorite',
+                bookCategory,
+            })
+
+        } catch (e) {
+            res.status(500).json({
+                message: 'Internal server error.',
+                error: e.message,
+            });
+        }
+    }
 }
--- schemas/books.js
+++ schemas/books.js
@@ -6,10 +6,20 @@ export default {
         author: Joi.string().trim().min(3).max(100).required(),
     }),
 
+    bookCategory: Joi.object({
+        bookId: Joi.number().integer().min(1).max(20).required(),
+        categoryId: Joi.number().integer().min(1).max(20).required(),
+    }),
+
     getBooks: Joi.object({
         page: Joi.number().integer().min(1).max(10000000).default(1).optional(),
         limit: Joi.number().integer().min(5).max(20).default(5).optional(),
         order: Joi.string().valid('asc', 'desc').default('desc').optional(),
         orderBy: Joi.string().valid('createdAt', 'updatedAt').default('createdAt').optional(),
-    })
+    }),
+
+    // searchBook: Joi.object({
+    //     title: Joi.string().trim().min(3).max(100).required(),
+    //     author: Joi.string().trim().min(3).max(100).required(),
+    // }),
 }
